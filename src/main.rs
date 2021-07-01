7c21dee36b99874d87bf8c45cfc4d40673494f7c
Teruteru <teruterubouzu1024@gmail.com>
2021-07-01 22:28:11 +0900

    Add tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
     println!("Hello, world!");
+    // https://www.rust-lang.org/ja/learn/get-started
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
